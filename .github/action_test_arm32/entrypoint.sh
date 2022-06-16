e3d7203f88b71f14271d7c9ebabea2b521929974
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2022-06-16 19:07:59 +0530

    Updated the Workflow

---
+++ .github/action_test_arm32/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+uname -a
+
+echo "Making build directory"
+mkdir build
+
+echo "Going into build directory"
+cd build
+
+echo "Generating cmake files"
+cmake ..
+
+echo "Running make"
+make -j 8
+
+echo "Running make install"
+make install
+
+echo "Running make package"
+make package
